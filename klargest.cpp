db9591e165c0b508028027126cc1dcc9e36d889c
AngusMcPherson <110000587+AngusMcPherson@users.noreply.github.com>
2023-05-22 21:13:35 +0930

    Add files via upload

---
+++ klargest.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+  
+
+int kth_largest(vector<int> values, int k){
+    
+    priority_queue<int> pq(values.begin(), values.end());
+    for (int i=0;i<k-1;i++){
+        pq.pop(); 
+    }
+    return pq.top();
+}
